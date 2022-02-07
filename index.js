c97436a108a4c35ebbc2d21908ed5a39e4fecc4f
ElmerPC <erey2790@gmail.com>
2022-02-07 01:27:37 -0600

    installed inquirer and added first manager question

--- index.js
+++ index.js
@@ -9,3 +9,32 @@ const Employee = require('./lib/Employee');
 
 const generatePage = require('./src/page-template');
 const writeFile = require('');
+
+const teamArray = [];
+
+// prompts
+const createManager = () => {
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the managers name?'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'Please enter the managers ID'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Please enter managers email'
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: 'Please enter the managers office number'
+            }
+        ])
+}
