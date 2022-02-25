31bcf00ad2f8416a6db0a3e04085022c56e3d39b
ElmerPC <erey2790@gmail.com>
2022-02-25 03:28:04 -0600

    index.html almost complete

--- src/page-template.js
+++ src/page-template.js
@@ -1,6 +1,75 @@
+const createTeam = team => {
+    // create manager index.html
+    const createManager = manager => {
+        return `<div class="card employee-card">
+        <div class="card-header bg-primary text-white">
+            <h2 class="card-title">${manager.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${manager.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                <li class="list-group-item">Office number: ${manager.getOffice()}</li>
+            </ul>
+        </div>
+    </div>`;
+    };
+
+    // create the index for engineers
+    const createEngineer = engineer => {
+        return `
+<div class="card employee-card">
+    <div class="card-header bg-primary text-white">
+        <h2 class="card-title">${engineer.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${engineer.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+        </ul>
+    </div>
+</div>
+        `;
+    };
+    // create intern for html
+    const createIntern = intern => {
+        return `
+<div class="card employee-card">
+    <div class="card-header bg-primary text-white">
+        <h2 class="card-title">${intern.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${intern.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+            <li class="list-group-item">School: ${intern.getSchool()}</li>
+        </ul>
+    </div>
+</div>
+        `;
+    };
+const index = [];
+index.push(team.filter(employee.getRole() === "Manager")
+.map(manager => createManager(manager)));
+
+index.push(team.filter(employee.getRole() === "Engineer")
+.map(engineer => createEngineer(engineer))
+.join(""));
+
+index.push(team.filter(employee => employee.getRole() === "Intern")
+.map(intern => createIntern(intern))
+.join("")
+);
+return index.join("")
+}
 
 
 module.exports = generatePage => {
+    console.log(generatePage);
     return `
         <!DOCTYPE html>
         <html lang="en">
@@ -14,17 +83,9 @@ module.exports = generatePage => {
             <header>
             <h1 class="text-center">My Team</h1>
             </header>
-            <div class="card" style="width: 18rem;">
-  <div class="card-header">
-    ${Manager.name}
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">An item</li>
-    <li class="list-group-item">A second item</li>
-    <li class="list-group-item">A third item</li>
-  </ul>
-</div>
-            ${(generatePage)}
+            
+           
+            ${createTeam}
         </body>
         </html>    
         `
