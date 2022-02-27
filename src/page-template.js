3004f2202fe28a0bf4df8c76303d16e76440b1ad
ElmerPC <erey2790@gmail.com>
2022-02-27 04:09:48 -0600

    added gen page

--- src/page-template.js
+++ src/page-template.js
@@ -1,3 +1,23 @@
+const generatePage = team => {
+
+    // create the manager html
+    const generateManager = manager => {
+        return `
+<div class="card employee-card">
+    <div class="card-header bg-primary text-white">
+        <h2 class="card-title">${manager.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${manager.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+            <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+        </ul>
+    </div>
+</div>
+        `;
+    };
 
 
 
@@ -27,7 +47,7 @@ module.exports = team => {
     <div class="container">
         <div class="row">
             <div class="row team-area col-12 d-flex justify-content-center">
-                ${generateTeam(team)}
+                ${generatePage(team)}
             </div>
         </div>
     </div>
